ab397a33e162bfcfbf13982df52397ec1f3b932a
Phitherek_ <phitherek@gmail.com>
2015-11-20 16:22:26 +0100

    About window launches, forced English locale because of program not being localized.

--- main.cpp
+++ main.cpp
@@ -4,11 +4,25 @@
 
 using namespace std;
 
+Gtk::ApplicationWindow* mainwindow;
+Gtk::AboutDialog* aboutdialog;
+Gtk::MenuItem* aboutitem;
+
+void onAboutMenuItemClick() {
+    aboutdialog->run();
+}
+
 int main(int argc, char** argv) {
+    Glib::setenv("LC_ALL", "C", true);
     Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "me.phitherek.oziexplorer_to_uiview_converter");
     Glib::RefPtr<Gtk::Builder> builder = Gtk::Builder::create_from_file("oziexplorer_uiview_converter.glade");
     builder->set_application(app);
-    Gtk::Window* mainwindow = NULL;
+    mainwindow = NULL;
     builder->get_widget("mainwindow", mainwindow);
+    aboutdialog = NULL;
+    builder->get_widget("aboutdialog", aboutdialog);
+    aboutitem = NULL;
+    builder->get_widget("aboutitem", aboutitem);
+    aboutitem->signal_activate().connect(sigc::ptr_fun(&onAboutMenuItemClick));
     return app->run(*mainwindow);
 }
