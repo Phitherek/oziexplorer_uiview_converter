ce2ebee00022b6629b0f98eb38c1d4a5076001c0
Phitherek_ <phitherek@gmail.com>
2015-11-20 19:41:28 +0100

    Dialogs cancel response is now handled.

--- main.cpp
+++ main.cpp
@@ -4,10 +4,14 @@
 
 using namespace std;
 
-Gtk::ApplicationWindow* mainwindow;
-Gtk::AboutDialog* aboutdialog;
-Gtk::MenuItem* aboutitem;
-Gtk::MenuItem* quititem;
+Gtk::ApplicationWindow* mainwindow = NULL;
+Gtk::AboutDialog* aboutdialog = NULL;
+Gtk::FileChooserDialog* inchooserdialog = NULL;
+Gtk::FileChooserDialog* outchooserdialog = NULL;
+Gtk::MenuItem* aboutitem = NULL;
+Gtk::MenuItem* quititem = NULL;
+Gtk::Button* inchooserbutton = NULL;
+Gtk::Button* outchooserbutton = NULL;
 
 void onAboutMenuItemClick() {
     aboutdialog->run();
@@ -22,20 +26,48 @@ void closeAboutDialog() {
     aboutdialog->close();
 }
 
+void openInChooserDialog() {
+    inchooserdialog->run();
+}
+
+void openOutChooserDialog() {
+    outchooserdialog->run();
+}
+
+void inChooserDialogResponse(int response_id) {
+    if(response_id == Gtk::RESPONSE_CANCEL || response_id == Gtk::RESPONSE_DELETE_EVENT) {
+        inchooserdialog->close();
+    }
+}
+
+void outChooserDialogResponse(int response_id) {
+    if(response_id == Gtk::RESPONSE_CANCEL || response_id == Gtk::RESPONSE_DELETE_EVENT) {
+        outchooserdialog->close();
+    }
+}
+
 int main(int argc, char** argv) {
     Glib::setenv("LC_ALL", "C", true);
     Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "me.phitherek.oziexplorer_to_uiview_converter");
     Glib::RefPtr<Gtk::Builder> builder = Gtk::Builder::create_from_file("oziexplorer_uiview_converter.glade");
     builder->set_application(app);
-    mainwindow = NULL;
     builder->get_widget("mainwindow", mainwindow);
-    aboutdialog = NULL;
     builder->get_widget("aboutdialog", aboutdialog);
-    aboutitem = NULL;
     builder->get_widget("aboutitem", aboutitem);
     aboutitem->signal_activate().connect(sigc::ptr_fun(&onAboutMenuItemClick));
-    quititem = NULL;
     builder->get_widget("quititem", quititem);
     quititem->signal_activate().connect(sigc::ptr_fun(&closeMainWindow));
+    builder->get_widget("inchooserdialog", inchooserdialog);
+    builder->get_widget("outchooserdialog", outchooserdialog);
+    builder->get_widget("inchooserbutton", inchooserbutton);
+    builder->get_widget("outchooserbutton", outchooserbutton);
+    inchooserdialog->add_button("Select", Gtk::RESPONSE_OK);
+    inchooserdialog->add_button("Cancel", Gtk::RESPONSE_CANCEL);
+    outchooserdialog->add_button("Select", Gtk::RESPONSE_OK);
+    outchooserdialog->add_button("Cancel", Gtk::RESPONSE_CANCEL);
+    inchooserbutton->signal_clicked().connect(sigc::ptr_fun(&openInChooserDialog));
+    outchooserbutton->signal_clicked().connect(sigc::ptr_fun(&openOutChooserDialog));
+    inchooserdialog->signal_response().connect(sigc::ptr_fun(&inChooserDialogResponse));
+    outchooserdialog->signal_response().connect(sigc::ptr_fun(&outChooserDialogResponse));
     return app->run(*mainwindow);
 }
