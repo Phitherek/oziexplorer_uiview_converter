c1d257d207dc2cffa947c70880935e8465f61df1
Phitherek_ <phitherek@gmail.com>
2015-11-20 15:47:24 +0100

    Main window launches.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include <cstdlib>
+#include <gtkmm-3.0/gtkmm.h>
 
 using namespace std;
 
-int main() {
-    return EXIT_SUCCESS;
+int main(int argc, char** argv) {
+    Glib::RefPtr<Gtk::Application> app = Gtk::Application::create(argc, argv, "me.phitherek.oziexplorer_to_uiview_converter");
+    Glib::RefPtr<Gtk::Builder> builder = Gtk::Builder::create_from_file("oziexplorer_uiview_converter.glade");
+    builder->set_application(app);
+    Gtk::Window* mainwindow = NULL;
+    builder->get_widget("mainwindow", mainwindow);
+    return app->run(*mainwindow);
 }
